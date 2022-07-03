17fd3c7a38dea7c539b3f9a0e6ebc8c3e12c28b3
MrPerry365 <1987nowhere@gmail.com>
2022-07-03 17:52:44 -0400

    add new routes to tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -13,8 +13,8 @@ router.get("/", (req, res) => {
       attributes: ["id", "product_name", "price", "stock", "category_id"],
     },
   })
-    .then((dbProductData) => {
-      res.json(dbProductData);
+    .then((dbTagData) => {
+      res.json(dbTagData);
     })
     .catch((err) => {
       console.log(err);
@@ -25,54 +25,70 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "tag_name"],
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+    },
+  })
 
-
-  // .then((dbProductData) => {
-  //   res.json(dbProductData);
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  //   res.status(500).json(err);
-  // });
+    .then((dbTagData) => {
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
 
-
-  // .then((dbProductData) => {
-  //   res.json(dbProductData);
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  //   res.status(500).json(err);
-  // });
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+  .then((dbTagData) => {
+    res.json(dbTagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 
-
-  // .then((dbProductData) => {
-  //   res.json(dbProductData);
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  //   res.status(500).json(err);
-  // });
+  Tag.update(
+    { tag_name: req.body.tag_name },
+    {where: {id: req.params.id,  }}
+  )
+  .then((dbTagData) => {
+    res.json(dbTagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
-
-
-  // .then((dbProductData) => {
-  //   res.json(dbProductData);
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  //   res.status(500).json(err);
-  // });
+  Tag.destroy({
+    where: {id: req.params.id},
+  })
+ 
+  .then((dbTagData) => {
+    res.json(dbTagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
